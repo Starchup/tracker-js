0d2cff3c91bb6f0b923e28a65ff6bb6322f7ccc4
Geoffroy Lesage <gefthefrench@gmail.com>
2017-07-02 11:02:00 +0300

    Added lock mechanism for requests

--- tracker.js
+++ tracker.js
@@ -1,6 +1,4 @@
-;
-(function (window, document, undefined)
-{
+;(function(window, document, undefined) {
     'use strict'
 
     var kDEVICE_ID_COOKIE = "tracker_device_id";
@@ -19,8 +17,7 @@
      *      Public API       *
      *                       *
      *************************/
-    var StarchupTracker = function (cleanerIdentifier, facilityId, source)
-    {
+    var StarchupTracker = function(cleanerIdentifier, facilityId, source) {
         globals.cleanerIdentifier = cleanerIdentifier;
         globals.facilityId = facilityId;
         globals.source = source;
@@ -30,9 +27,17 @@
 
         return this;
     }
-    var trackEvent = function (event, cb)
-    {
+    var trackEvent = function(event, cb) {
+        checkAndEngageLock(function() {
+            trackEvent(event, cb);
+        });
+
         if (!cb || cb === undefined) cb = backupCb;
+
+        function done(err, res) {
+            disengageLock();
+            cb(err, res);
+        }
 
         if (!event) throwError("Missing event");
 
@@ -52,23 +57,27 @@
         if (globals.facilityId) event.facilityId = globals.facilityId;
         if (globals.source) event.source = globals.source;
 
-        if (!globals.deviceId)
-        {
-            createDevice(function (err, device)
-            {
-                if (!device) cb();
-                else
-                {
+        if (!globals.deviceId) {
+            createDevice(function(err, device) {
+                if (err || !device) done();
+                else {
                     event.deviceId = device.id;
-                    request("POST", "Events", event, cb);
+                    request("POST", "Events", event, done);
                 }
             });
-        }
-        else request("POST", "Events", event, cb);
+        } else request("POST", "Events", event, done);
     };
-    var logIn = function (customerId, agentId, token, cb)
-    {
+    var logIn = function(customerId, agentId, token, cb) {
+        checkAndEngageLock(function() {
+            logIn(customerId, agentId, token, cb);
+        });
+
         if (!cb || cb === undefined) cb = backupCb;
+
+        function done(err, res) {
+            disengageLock();
+            cb(err, res);
+        }
 
         if (!customerId && !agentId) throwError("Missing user Id");
         if (!token) throwError("Missing token");
@@ -77,20 +86,19 @@
         globals.customerId = customerId;
         globals.agentId = agentId;
 
-        var callback = function (err, res)
-        {
-            if (err || !res) return cb();
+        if (globals.deviceId) return done();
+
+        var callback = function(err, res) {
+            if (err || !res) return done();
 
             var data = {
                 customerId: customerId,
                 agentId: agentId
             };
-            if (res && res.length > 0)
-            {
+            if (res && res.length > 0) {
                 var firstId = res[0]["id"];
                 data.deviceId = res[0]["deviceId"];
-                res.forEach(function (d)
-                {
+                res.forEach(function(d) {
                     if (d.customerId != globals.customerId) return;
                     if (d.agentId != globals.agentId) return;
                     if (!d.id || !d.deviceId) return;
@@ -101,13 +109,12 @@
                 });
                 if (!globals.deviceId) globals.deviceId = firstId;
             }
-            request("PUT", "DeviceData/" + globals.deviceId, data, cb);
+            request("PUT", "DeviceData/" + globals.deviceId, data, done);
         };
         if (customerId) getCustomerDevices(callback);
         else if (agentId) getAgentDevices(callback);
     };
-    var logOut = function ()
-    {
+    var logOut = function() {
         globals.customerId = null;
         globals.deviceId = null;
         globals.access_token = null;
@@ -131,46 +138,46 @@
      *    Private Helpers    *
      *                       *
      *************************/
-    var backupCb = function () {}
+    var backupCb = function() {}
 
-    var getDevices = function (where, cb)
-    {
+    var checkAndEngageLock = function(callback) {
+        if (globals.busy) window.setTimeout(function() {
+            callback();
+        }, 100);
+        globals.busy = true;
+    }
+    var disengageLock = function() {
+        globals.busy = false;
+    }
+
+    var getDevices = function(where, cb) {
         if (!cb || cb === undefined) cb = backupCb;
         var query = {
             "where": where
         };
         request("GET", "DeviceData", query, cb);
     }
-    var getCustomerDevices = function (cb)
-    {
+    var getCustomerDevices = function(cb) {
         if (!globals.customerId) throwError("Missing customerId");
-        getDevices(
-        {
+        getDevices({
             customerId: globals.customerId,
             source: globals.source
         }, cb);
     };
-    var getAgentDevices = function (cb)
-    {
+    var getAgentDevices = function(cb) {
         if (!globals.agentId) throwError("Missing agentId");
-        getDevices(
-        {
+        getDevices({
             "agentId": globals.agentId,
             source: globals.source
         }, cb);
     };
-    var getApiBasedOnHost = function ()
-    {
+    var getApiBasedOnHost = function() {
         var domain = window.location.hostname;
         if (window.location.port.length > 0) domain = domain.replace(":" + window.location.port, "");
-        if (domain === "localhost") return "http://dev.starchup.com:3005/api";
-        else if (domain === "dev.starchup.com") return "http://dev.starchup.com:3005/api";
-        else if (domain === "stage.starchup.com") return "https://sandtrack.starchup.com/api";
-        else return "https://tracker.starchup.com/api";
+        return "https://tracker.starchup.com/api";
     }
 
-    var request = function (method, resource, data, cb)
-    {
+    var request = function(method, resource, data, cb) {
         if (!cb || cb === undefined) cb = backupCb;
 
         if (!method) throwError("Missing method");
@@ -182,21 +189,17 @@
 
         if (data) data = JSON.stringify(data);
 
-        if (globals.access_token)
-        {
+        if (globals.access_token) {
             requestURL = requestURL + "?access_token=" + globals.access_token;
         }
 
-        if (method === "GET" && data)
-        {
+        if (method === "GET" && data) {
             requestURL = requestURL + (globals.access_token ? "&" : "?") + "filter=" + data;
         }
 
         var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function ()
-        {
-            if (this.readyState === 4)
-            {
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4) {
                 if (this.status != 200) cb();
                 else cb(null, JSON.parse(this.responseText));
             }
@@ -206,8 +209,8 @@
         xhttp.send(method !== "GET" ? data : null);
     };
 
-    function throwError(message)
-    {
+    function throwError(message) {
+        globals.busy = false;
         throw new Error('StarchupTracker --- ' + message)
     }
 
@@ -216,8 +219,7 @@
      * Device data utilities *
      *                       *
      *************************/
-    var createDevice = function (cb)
-    {
+    var createDevice = function(cb) {
         if (!cb || cb === undefined) cb = backupCb;
 
         var dd = getBrowserData();
@@ -229,20 +231,16 @@
         dd.ip = findIP();
         dd.identifier = globals.cleanerIdentifier;
 
-        request("POST", "DeviceData", dd, function (err, res)
-        {
-            if (res && res.id)
-            {
+        request("POST", "DeviceData", dd, function(err, res) {
+            if (res && res.id) {
                 globals.deviceId = res.id;
                 setCookie(kDEVICE_ID_COOKIE, res.id, 0.5);
             }
             cb(err, res);
         });
     };
-    var guid = function ()
-    {
-        function s4()
-        {
+    var guid = function() {
+        function s4() {
             return Math.floor((1 + Math.random()) * 0x10000)
                 .toString(16)
                 .substring(1);
@@ -251,8 +249,7 @@
             s4() + '-' + s4() + s4() + s4();
     }
 
-    function findIP()
-    {
+    function findIP() {
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.open("GET", "https://api.ipify.org/", false);
         xmlHttp.send(null);
@@ -264,89 +261,75 @@
      * Browser data utilities *
      *                        *
      **************************/
-    var getURLParamByName = function (name)
-    {
+    var getURLParamByName = function(name) {
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
         var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
             results = regex.exec(location.search);
         return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     }
 
-    function setCookie(cname, cvalue, exdays)
-    {
+    function setCookie(cname, cvalue, exdays) {
         var d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
         var expires = "expires=" + d.toUTCString();
         document.cookie = cname + "=" + cvalue + "; " + expires;
     }
 
-    function getCookie(cname)
-    {
+    function getCookie(cname) {
         var name = cname + "=";
         var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++)
-        {
+        for (var i = 0; i < ca.length; i++) {
             var c = ca[i];
-            while (c.charAt(0) == ' ')
-            {
+            while (c.charAt(0) == ' ') {
                 c = c.substring(1);
             }
-            if (c.indexOf(name) == 0)
-            {
+            if (c.indexOf(name) == 0) {
                 return c.substring(name.length, c.length);
             }
         }
         return "";
     }
-    var getBrowserData = function ()
-    {
+    var getBrowserData = function() {
         var nAgt = navigator.userAgent;
         var browserName = navigator.appName;
         var fullVersion = '' + parseFloat(navigator.appVersion);
         var nameOffset, verOffset, ix;
 
         // In Opera, the true version is after "Opera" or after "Version"
-        if ((verOffset = nAgt.indexOf("Opera")) != -1)
-        {
+        if ((verOffset = nAgt.indexOf("Opera")) != -1) {
             browserName = "Opera";
             fullVersion = nAgt.substring(verOffset + 6);
             if ((verOffset = nAgt.indexOf("Version")) != -1)
                 fullVersion = nAgt.substring(verOffset + 8);
         }
         // In MSIE, the true version is after "MSIE" in userAgent
-        else if ((verOffset = nAgt.indexOf("MSIE")) != -1)
-        {
+        else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
             browserName = "Microsoft Internet Explorer";
             fullVersion = nAgt.substring(verOffset + 5);
         }
         // In Chrome, the true version is after "Chrome" 
-        else if ((verOffset = nAgt.indexOf("Chrome")) != -1)
-        {
+        else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
             browserName = "Chrome";
             fullVersion = nAgt.substring(verOffset + 7);
         }
         // In Safari, the true version is after "Safari" or after "Version" 
-        else if ((verOffset = nAgt.indexOf("Safari")) != -1)
-        {
+        else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
             browserName = "Safari";
             fullVersion = nAgt.substring(verOffset + 7);
             if ((verOffset = nAgt.indexOf("Version")) != -1)
                 fullVersion = nAgt.substring(verOffset + 8);
         }
         // In Firefox, the true version is after "Firefox" 
-        else if ((verOffset = nAgt.indexOf("Firefox")) != -1)
-        {
+        else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
             browserName = "Firefox";
             fullVersion = nAgt.substring(verOffset + 8);
         }
         // In most other browsers, "name/version" is at the end of userAgent 
         else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) <
-            (verOffset = nAgt.lastIndexOf('/')))
-        {
+            (verOffset = nAgt.lastIndexOf('/'))) {
             browserName = nAgt.substring(nameOffset, verOffset);
             fullVersion = nAgt.substring(verOffset + 1);
-            if (browserName.toLowerCase() == browserName.toUpperCase())
-            {
+            if (browserName.toLowerCase() == browserName.toUpperCase()) {
                 browserName = navigator.appName;
             }
         }
@@ -363,8 +346,7 @@
         };
     }
 
-    var getOS = function ()
-    {
+    var getOS = function() {
         if (navigator.appVersion.indexOf("Win") != -1) return "Windows";
         if (navigator.appVersion.indexOf("Mac") != -1) return "MacOS";
         if (navigator.appVersion.indexOf("X11") != -1) return "UNIX";
